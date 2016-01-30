b864f7909789489f63c33e6d719503ca3cac069a
Saleh Albatati <0xack13@gmail.com>
2016-01-30 15:05:46 +0300

    Initial Commit

---
+++ ActiveApplication/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ActiveApplication
+//
+//  Created by 0xack13 on 1/30/16.
+//  Copyright © 2016 0xack13. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
