55594011ae7d18549906d94210f47be7625a7231
Saleh Albatati <0xack13@gmail.com>
2016-01-30 15:56:10 +0300

    List all process

--- ActiveApplication/main.m
+++ ActiveApplication/main.m
@@ -7,11 +7,40 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+#include<unistd.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+@interface Foo : NSObject
+- (void)run;
+@end
+
+@implementation Foo
+- (void)run {
+    for (NSRunningApplication *currApp in [[NSWorkspace sharedWorkspace] runningApplications]) {
+        if ([currApp isActive]) {
+            NSLog(@"* %@. Is Active: %@", [currApp localizedName], [currApp isActive]);
+        } else {
+            NSLog(@"  %@", [currApp localizedName]);
+        }
     }
-    return 0;
+    NSLog(@"---");
+}
+@end
+
+int main(int argc, char *argv[]) {
+    
+    sleep(3);
+    
+    //NSAutoreleasePool *p = [NSAutoreleasePool new];
+    
+    //Foo *foo = [[Foo new] autorelease];
+    Foo *foo = [Foo new];
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                                      target:foo
+                                                    selector:@selector(run)
+                                                    userInfo:nil
+                                                     repeats:NO];
+    [[NSRunLoop mainRunLoop] run];
+    //Foo.run();
+    //[p release];
 }
